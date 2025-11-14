c0590316f050582d7fa3c00bf4e13dab3e2d4293
ItsYeeBoi <76560929+ItsYeeBoi@users.noreply.github.com>
2025-11-14 01:57:47 +0000

    fix(icon): remove double
    Remove double icon path

--- src/pyclickr/app.py
+++ src/pyclickr/app.py
@@ -153,7 +153,6 @@ class App:
         listener.start()
 
     def setup_gui(self):
-        self.icon_path = self.resource_path(r"assets\PyClickr_Icon.ico")
         self.icon_path = self.resource_path(r"assets\PyClickr.ico")
         dpg.create_context()
 
